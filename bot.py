2f0ba162295dd94804a7f9d00bf37f47b478e86a
Nathan Glick <nxg9997@rit.edu>
2020-07-25 15:36:39 -0400

    cooldown and quote library

--- bot.py
+++ bot.py
@@ -1,8 +1,13 @@
 # Perm Int: 3406912
+
+# Import Libraries / Modules
 import discord
 import random
 
 import os
+
+# Only import Config.py if the bot is being run locally
+# 'HOSTED' is a environment variable set on Heroku that equals the bot token
 token = ''
 if os.environ.get('HOSTED') == None:
     import config
@@ -11,32 +16,44 @@ else:
     token = str(os.environ.get('HOSTED'))
     print('Running on Heroku, token = ' + token)
 
-scopebook = [
-    "**- SCOPE -**",
-    "*But have you considered... scope?*",
-    "I like your idea, but, it's a little outside the scope 乁| ･ 〰 ･ |ㄏ",
-    "https://pmweb.com/wordpress/wp-content/uploads/2017/12/Scope-blog-Joao.jpg"
-]
+import data
 
+# create a Discord client instance
 client = discord.Client()
 
+# Discord Events
+
+# When the bot is successfully logged in
 @client.event
 async def on_ready():
     print('Logged in as {0.user}'.format(client))
 
+# When a message is sent to a channel visible to the bot
 @client.event
 async def on_message(msg):
+
+    # enforces cooldown on sending scope messages
+    if not str(msg.channel.id) in data.cooldowns:
+        data.cooldowns[str(msg.channel.id)] = 0
+    elif data.cooldowns[str(msg.channel.id)] > 0:
+        data.cooldowns[str(msg.channel.id)] -= 1
+
+    # ignore messages sent by the bot
     if msg.author == client.user:
         return
+    
+    # 'hello' ping
     if msg.content.startswith('$hello'):
         await msg.channel.send('Hello!')
 
+    # send a scope reminder (by using a command, random chance, or by containing a keyword)
     if msg.content.startswith('!scope'):
-        await msg.channel.send(scopebook[random.randint(0,len(scopebook)-1)])
-    elif random.randint(0,100) <= 10:
-        await msg.channel.send(scopebook[random.randint(0,len(scopebook)-1)])
+        await msg.channel.send(data.scopebook[random.randint(0,len(data.scopebook)-1)])
+    elif (random.randint(0,100) <= 10 or any(map(msg.content.__contains__,data.keywords))) and data.cooldowns[str(msg.channel.id)] == 0:
+        data.cooldowns[str(msg.channel.id)] = 10
+        await msg.channel.send(data.scopebook[random.randint(0,len(data.scopebook)-1)])
 
     
-
+# run the bot
 print('Bot online')
 client.run(token)
---
+++ data.py
@@ -0,0 +1,32 @@
+
+# Trigger words or phrases
+keywords = [
+    "feature",
+    "it would be cool if",
+    "idea",
+    "let's make",
+    "let's add",
+    "what if",
+    "project",
+    "i had a thought",
+    "i was thinking",
+    "talking about",
+    "new"
+]
+
+# Possible phrases the bot can say about scope
+scopebook = [
+    "**- SCOPE -**",
+    "*But have you considered... scope?*",
+    "I like your idea, but, it's a little outside the scope 乁| ･ 〰 ･ |ㄏ",
+    "https://pmweb.com/wordpress/wp-content/uploads/2017/12/Scope-blog-Joao.jpg",
+    "I’m Commander Scope, and this is my favorite out of scope idea on the server!",
+    "It’s time to kick ass and make a game ... and we're outta the scope.",
+    "You were almost a scope sandwich!",
+    "It’s a-me, Scope!",
+    "Would you kindly... think about scope?",
+    "Scope... scope never changes"
+]
+
+# channel cooldowns
+cooldowns = {}
